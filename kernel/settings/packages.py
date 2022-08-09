bf96b401eba87cf7db64c0ff8b8b940a7e8416d2
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-08-09 11:50:05 +0430

    initialize project

---
+++ kernel/settings/packages.py
@@ -0,0 +1,41 @@
+import os
+from datetime import timedelta
+
+from decouple import config
+from django.conf import settings
+
+from .base import (
+    INSTALLED_APPS, MIDDLEWARE,
+    STATIC_ROOT, BASE_DIR, STATICFILES_DIRS
+)
+
+# ############## #
+#   EXTENSIONS   #
+# ############## #
+
+# admin
+INSTALLED_APPS.append('django.contrib.admindocs')
+INSTALLED_APPS.append('django.contrib.sites')
+
+# packages
+INSTALLED_APPS.append('admin_footer')
+INSTALLED_APPS.append('admin_honeypot')
+
+# Log
+
+# Security
+
+# Applications
+
+# ########### #
+#   UPLOAD    #
+# ########### #
+# FILE_UPLOAD_HANDLERS = [
+#     'painless.utils.handlers.upload.ChunkFileUploadHandler'
+# ]
+# UPLOAD_CHUNK_SIZE = 2500 * 2 ** 10  # 2500 KB
+
+# #################### #
+# IMPORTANT VARIABLES  #
+# #################### #
+# AUTH_USER_MODEL = 'accounts.CustomUser'
